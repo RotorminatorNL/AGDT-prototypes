c91220f5997fb32d276096083f127cd82c186548
Robbert Lengton <robbert.lengton@hotmail.nl>
2023-02-28 11:51:25 +0100

    Added Odin shit

---
+++ project-to-prototype/Assets/Scripts/Odin/OdinScript.cs
@@ -0,0 +1,10 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OdinScript : MonoBehaviour
+{
+    [Title("Sweg", Subtitle = "Dikzakken")]
+    public float sweg;
+}
